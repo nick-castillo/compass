ef95a2887b0cead1099a86c23f5a01e0d3b997e5
Nick <cstllnick@gmail.com>
2019-11-08 20:35:19 -0700

    Documented the get_user_ip method and removed the php_unit cache file

--- includes/class-compass-user.php
+++ includes/class-compass-user.php
@@ -13,21 +13,28 @@ namespace Compass;
  * @subpackage Compass/includes
  */
 class Compass_User {
+    /**
+     * Grab the user's IP adress
+     *
+     * @since   1.0.0
+     * @return  string  The user's IP address or null 
+     */
     public function get_ip() {
         $ipaddress = null;
 
-        if (isset($_SERVER['HTTP_CLIENT_IP']))
+        if (isset($_SERVER['HTTP_CLIENT_IP'])) {
             $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        } else if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
             $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        } else if(isset($_SERVER['HTTP_X_FORWARDED'])) {
             $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        } else if(isset($_SERVER['HTTP_FORWARDED_FOR'])) {
             $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        else if(isset($_SERVER['HTTP_FORWARDED']))
+        } else if(isset($_SERVER['HTTP_FORWARDED'])) {
             $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        else if(isset($_SERVER['REMOTE_ADDR']))
+        } else if(isset($_SERVER['REMOTE_ADDR'])) {
             $ipaddress = $_SERVER['REMOTE_ADDR'];
+        }
 
         return $ipaddress;
     }
