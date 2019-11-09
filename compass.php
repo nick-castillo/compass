f18ba4cc8195aa1f28010bf3ac092799f6fd9f0a
Nick <cstllnick@gmail.com>
2019-11-08 17:08:20 -0700

    Finished cleanup and added composer

--- compass.php
+++ compass.php
@@ -38,45 +38,10 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'COMPASS_VERSION', '1.0.0' );
 
 /**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-compass-activator.php
+ * Autoload the plugin classes
  */
-function activate_compass() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-compass-activator.php';
-	Compass_Activator::activate();
+function load_compass() {
+	require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 }
 
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-compass-deactivator.php
- */
-function deactivate_compass() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-compass-deactivator.php';
-	Compass_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_compass' );
-register_deactivation_hook( __FILE__, 'deactivate_compass' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-compass.php';
-
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_compass() {
-
-	$plugin = new Compass();
-	$plugin->run();
-
-}
-run_compass();
+add_action( 'plugins_loaded', 'load_compass' );
--- includes/class-compass.php
+++ includes/class-compass.php
@@ -1,5 +1,7 @@
 <?php
 
+use Compass\Compass_Loader;
+
 /**
  * The file that defines the core plugin class
  *
@@ -28,16 +30,6 @@
  * @author     Nick Castillo <cstllnick@gmail.com>
  */
 class Compass {
-
-	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      Compass_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
-	protected $loader;
 
 	/**
 	 * The unique identifier of this plugin.
@@ -72,68 +64,12 @@ class Compass {
 		} else {
 			$this->version = '1.0.0';
 		}
+
 		$this->plugin_name = 'compass';
 
 		$this->load_dependencies();
 		$this->define_public_hooks();
 
-	}
-
-	/**
-	 * Load the required dependencies for this plugin.
-	 *
-	 * Include the following files that make up the plugin:
-	 *
-	 * - Compass_Loader. Orchestrates the hooks of the plugin.
-	 * - Compass_Public. Defines all hooks for the public side of the site.
-	 *
-	 * Create an instance of the loader which will be used to register the hooks
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function load_dependencies() {
-
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-compass-loader.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-compass-public.php';
-
-		$this->loader = new Compass_Loader();
-
-	}
-
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Compass_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
-	}
-
-	/**
-	 * Run the loader to execute all of the hooks with WordPress.
-	 *
-	 * @since    1.0.0
-	 */
-	public function run() {
-		$this->loader->run();
 	}
 
 	/**
@@ -145,16 +81,6 @@ class Compass {
 	 */
 	public function get_plugin_name() {
 		return $this->plugin_name;
-	}
-
-	/**
-	 * The reference to the class that orchestrates the hooks with the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    Compass_Loader    Orchestrates the hooks of the plugin.
-	 */
-	public function get_loader() {
-		return $this->loader;
 	}
 
 	/**
